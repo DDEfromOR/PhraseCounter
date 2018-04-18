34b347a2ebc8e79cd50734bf5b58a293b3d0e856
Daniel Evans <Daniel@ddevans.com>
2018-04-17 20:04:47 -0700

    Better naming and handling of no input

--- PhraseCounter.rb
+++ PhraseCounter.rb
@@ -1,11 +1,15 @@
 require "pry"
 
     def main
-        input = ARGF.read.downcase.scan(/(\b[a-z]+\b)/).map 
-        Counter(input).each{|key| Pry::ColorPrinter.pp("#{key[1]} - #{key[0][0]} #{key[0][1]} #{key[0][2]}")}
+        if ARGF.filename != "-" or (not STDIN.tty? and not STDIN.closed?)
+            input = ARGF.read.downcase.scan(/(\b[a-z]+\b)/).map 
+            counter(input).each{|key| Pry::ColorPrinter.pp("#{key[1]} - #{key[0][0]} #{key[0][1]} #{key[0][2]}")}
+        else
+            Pry::ColorPrinter.pp("Could not find any phrases to count!")
+        end
     end
 
-    def Counter (words)
+    def counter (words)
         phrases = Hash.new(0)
         words.each_cons(3) do |word1, word2, word3|
             phrase = word1 + word2 + word3
@@ -14,5 +18,5 @@ require "pry"
     
         phrases.sort_by {|key,value| value}.last(100).reverse
     end
-    
+        
 main
