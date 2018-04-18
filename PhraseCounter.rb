ec7cd72be995417625ef353f95c809174291a8a1
Daniel Evans <Daniel@ddevans.com>
2018-04-17 19:22:37 -0700

    Overhaul for better testing and naming conventions

--- PhraseCounter.rb
+++ PhraseCounter.rb
@@ -1,15 +1,18 @@
 require "pry"
 
-class PhraseCounter
-    counter = Hash.new(0)
-    words = ARGF.read.downcase.scan(/(\b[a-z]+\b)/).map 
+    def main
+        input = ARGF.read.downcase.scan(/(\b[a-z]+\b)/).map 
+        Counter(input).each{|key| Pry::ColorPrinter.pp("#{key[1]} - #{key[0][0]} #{key[0][1]} #{key[0][2]}")}
+    end
+
+    def Counter (words)
+        phrases = Hash.new(0)
+        words.each_cons(3) do |word1, word2, word3|
+            phrase = word1 + word2 + word3
+            phrases[phrase] = phrases[phrase] + 1
+        end
     
-    words.each_cons(3) do |word1, word2, word3|
-        phrase = word1 + word2 + word3
-        counter[phrase] = counter[phrase] + 1
+        phrases.sort_by {|key,value| value}.last(100).reverse
     end
     
-    sortedCounter = counter.sort_by {|key,value| value}
-    sortedCounter.last(100).reverse
-    .each{|key| Pry::ColorPrinter.pp("#{key[1]} - #{key[0][0]} #{key[0][1]} #{key[0][2]}")}
-end
+main
