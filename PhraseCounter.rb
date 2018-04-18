71198994ce2720ca35ec6ec2a45ac24f03e49624
Daniel Evans <Daniel@ddevans.com>
2018-04-17 21:55:11 -0700

    Pretty print isn't really doing anything cool enough to use here

--- PhraseCounter.rb
+++ PhraseCounter.rb
@@ -1,22 +1,20 @@
-require "pry"
+def main
+    if ARGF.filename != "-" or (not STDIN.tty? and not STDIN.closed?)
+        input = ARGF.read.downcase.scan(/(\b[a-z]+\b)/).map 
+        counter(input).each{|key| puts ("#{key[1]} - #{key[0][0]} #{key[0][1]} #{key[0][2]}")}
+    else
+        puts ("Could not find any phrases to count!")
+    end
+end
 
-    def main
-        if ARGF.filename != "-" or (not STDIN.tty? and not STDIN.closed?)
-            input = ARGF.read.downcase.scan(/(\b[a-z]+\b)/).map 
-            counter(input).each{|key| Pry::ColorPrinter.pp("#{key[1]} - #{key[0][0]} #{key[0][1]} #{key[0][2]}")}
-        else
-            Pry::ColorPrinter.pp("Could not find any phrases to count!")
-        end
+def counter (words)
+    phrases = Hash.new(0)
+    words.each_cons(3) do |word1, word2, word3|
+        phrase = word1 + word2 + word3
+        phrases[phrase] = phrases[phrase] + 1
     end
 
-    def counter (words)
-        phrases = Hash.new(0)
-        words.each_cons(3) do |word1, word2, word3|
-            phrase = word1 + word2 + word3
-            phrases[phrase] = phrases[phrase] + 1
-        end
+    phrases.sort_by {|key,value| value}.last(100).reverse
+end
     
-        phrases.sort_by {|key,value| value}.last(100).reverse
-    end
-        
 main
