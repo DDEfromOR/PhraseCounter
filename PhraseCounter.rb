c8fd62616c65b832c7da9dbce37f673e2eff46f9
Daniel Evans <Daniel@ddevans.com>
2018-04-18 20:24:35 -0700

    Moving the work into the counter function and out of main

--- PhraseCounter.rb
+++ PhraseCounter.rb
@@ -1,7 +1,10 @@
 def main
     if ARGF.filename != "-" or (not STDIN.tty? and not STDIN.closed?)
-        input = ARGF.read.downcase.scan(/(\b[a-z]+\b)/).map 
-        counter(input).each{|key| puts ("#{key[1]} - #{key[0][0]} #{key[0][1]} #{key[0][2]}")}
+        input = ARGF.read
+        unless input.length > 2
+            puts ("Input contains less than three words!")
+        end
+        counter(input)
     else
         puts ("Could not find any phrases to count!")
     end
@@ -9,12 +12,13 @@ end
 
 def counter (words)
     phrases = Hash.new(0)
-    words.each_cons(3) do |word1, word2, word3|
+    words.downcase.scan(/(\b[a-z]+\b)/).map.each_cons(3) do |word1, word2, word3|
         phrase = word1 + word2 + word3
         phrases[phrase] = phrases[phrase] + 1
     end
 
     phrases.sort_by {|key,value| value}.last(100).reverse
+      .each{|key| puts ("#{key[1]} - #{key[0][0]} #{key[0][1]} #{key[0][2]}")}
 end
     
 main
